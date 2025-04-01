fd9e81cc1fee3004e7178419ee1798b1650b011d
João Vitor Gonçalves da Costa <goncalvesdacosta05@gmail.com>
2025-04-01 20:51:57 -0300

    Fix: changed how the timer works

--- timer-simples/src/App.tsx
+++ timer-simples/src/App.tsx
@@ -1,26 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import Timer from './components/Timer'
 
 function App() {
-  const [time, setTime] = useState(0)
-  const [renderTimer, setRenderTimer] = useState(false);
+  const [runTime, setRunTime] = useState<1 | 2>(1);
+  const [currentTime, setCurrentTime] = useState(0);
+  
+  function handleTimerStart() {
+    if (runTime == 1) {
+      setRunTime(2);
+    } else if (runTime == 2) {
+      setRunTime(1);
+    }
 
+    if (currentTime > 0) {
+      setCurrentTime(0);
+    }
+  }
+  
+    
+  useEffect(() => {
+    if (runTime == 1) {
+      return;
+    } else if (runTime == 2) {
+      const interval = setInterval(() => {
+        setCurrentTime(time => time + 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [runTime]);
+  
   return (
     <>
       <div className='container-fluid bg-dark text-white d-flex justify-content-center align-items-center flex-column'>
-        <div>
-          <label htmlFor="time" className='fs-6 text-start'>Tempo em segundos: </label>
-          <input value={time} onChange={(e) => setTime(+e.target.value)} type="number" id='time' className='form-control input' />
-        </div>
         <div className='d-flex justify-content-center gap-3 align-items-center'>
-          <button onClick={() => setRenderTimer(false)} className='btn btn-danger my-3 '> Cancelar </button>
-          <button onClick={() => setRenderTimer(true)} className='btn btn-primary my-3'> Iniciar </button>
+          <button onClick={() => handleTimerStart()} className='btn btn-primary my-3'> { runTime == 1 && currentTime == 0 ? 'Iniciar' : 'Reiniciar' } </button>
         </div>
 
-        {
-          renderTimer ? <Timer time={time}/> : null
-        }
+        <p className="fs-6">Tempo atual: { currentTime }</p>
       </div>
     </>
   )
