9ee6ec642c922e9a9c28ae4f6f3f449d06d5bce5
João Vitor Gonçalves da Costa <goncalvesdacosta05@gmail.com>
2025-04-01 20:02:13 -0300

    Updated the layout of the 'componentizacao' task

--- componentizacao/src/App.tsx
+++ componentizacao/src/App.tsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import AsideMenu from './components/aside-menu/AsideMenu'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='container-fluid bg-dark text-white px-0'>
       <Header />
-      <div className='row mx-0 px-0'>
+      <div className='row mx-0 pe-4 ps-0'>
         <AsideMenu classes="col-3" />
-        <p className="col-9">
-          conteudo
+        <p className="col-9 mt-3 bg-primary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ipsa vitae quos repellat perferendis soluta suscipit quasi excepturi esse explicabo ea iste nisi, corporis in amet porro? Tenetur assumenda neque tempora incidunt architecto ab, inventore libero laudantium ad ducimus provident expedita eos voluptate culpa porro vero perspiciatis fugit quod dolorum eius vitae excepturi repellat. Quisquam repellat expedita alias autem similique deleniti corporis consequuntur, dolorem mollitia sapiente quia soluta eligendi pariatur, temporibus minima! Labore laboriosam nesciunt cupiditate, minima illum pariatur consequatur tempora numquam quidem. Libero facilis maiores impedit necessitatibus alias voluptatem nostrum quo, consequatur odit nihil! Incidunt ipsum dolores odio omnis accusantium placeat animi distinctio sed in, tempora totam sunt ea quaerat! Eveniet nihil autem ducimus magni distinctio sint, obcaecati dignissimos consequatur aut excepturi! Earum perferendis a necessitatibus ut quidem nisi, ipsum aspernatur voluptates dolore cum est exercitationem voluptas reiciendis culpa accusamus inventore porro perspiciatis neque consectetur tempore animi deleniti. Ex magni explicabo beatae. Ullam dicta exercitationem quaerat a rem id distinctio eligendi, blanditiis ut reiciendis nulla amet maxime vitae, impedit quam. Quos, temporibus ullam asperiores eos optio tenetur magni quibusdam earum molestiae debitis vero cum voluptate nemo, numquam odio nobis. Nisi necessitatibus quaerat provident aliquam quo quae cum reprehenderit consectetur!
         </p>
       </div>
     </div>
--- componentizacao/src/components/Header.tsx
+++ componentizacao/src/components/Header.tsx
@@ -2,14 +2,14 @@ import Logo from "./Logo";
 import "./header.css"
 
 export default function Header() {
-  return <header className="w-100 header bg-white d-flex justify-content-between align-items-center py-3">
+  return <header className="w-100 header bg-white d-flex justify-content-between align-items-center py-3 px-3">
     <Logo />
     <div>
       <button className="btn btn-primary">
         Sign in
       </button>
 
-      <button className="btn btn-light">
+      <button className="btn btn-light ms-3">
         Sign up
       </button>
     </div>
